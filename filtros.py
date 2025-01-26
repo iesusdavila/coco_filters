29ea585ea935e45e38bf52dbf3943f57e978e55e
Iesus Davila <iesusdavila@gmail.com>
2025-01-25 22:41:30 -0500

    Reconstruction through OOP

--- filtros.py
+++ filtros.py
@@ -1,70 +1,64 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('/home/iesus_robot/opencv/data/haarcascades/haarcascade_frontalface_alt.xml')
+class SombreroFiltro:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier('/home/iesus_robot/opencv/data/haarcascades/haarcascade_frontalface_alt.xml')
+        self.sombrero_paths = ["imgs/hats/hat1.png", "imgs/hats/hat2.png", "imgs/hats/hat3.png", "imgs/hats/hat4.png"]
+        self.sombrero_imgs = [cv2.imread(path, cv2.IMREAD_UNCHANGED) for path in self.sombrero_paths]
+        self.current_sombrero_index = 1
+        self.sombrero_img, self.alpha = self.cargar_sombrero(self.current_sombrero_index)
+        self.historial_rostros = []
 
-sombrero_paths = ["imgs/hats/hat1.png", "imgs/hats/hat2.png", "imgs/hats/hat3.png", "imgs/hats/hat4.png"]
-sombrero_imgs = [cv2.imread(path, cv2.IMREAD_UNCHANGED) for path in sombrero_paths]
-current_sombrero_index = 1
+    def cargar_sombrero(self, index):
+        sombrero_img = self.sombrero_imgs[index]
+        alpha = cv2.split(sombrero_img)[3] / 255.0  # Normalizar el canal alfa
+        return sombrero_img, alpha
 
-def cargar_sombrero(index):
-    sombrero_img = sombrero_imgs[index]
-    alpha = cv2.split(sombrero_img)[3] / 255.0  # Normalizar el canal alfa
-    return sombrero_img, alpha
+    def agregar_sombrero(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rostros = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-sombrero_img, alpha = cargar_sombrero(current_sombrero_index)
+        if len(rostros) > 0:
+            self.historial_rostros.append(rostros)
+        else:
+            self.historial_rostros.append([])
 
-historial_rostros = []
+        if len(self.historial_rostros) > 5:
+            self.historial_rostros.pop(0)
 
-def agregar_sombrero(frame, sombrero_img, alpha):
-    global historial_rostros
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    rostros = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        rostros_suavizados = []
+        for i in range(len(rostros)):
+            x, y, w, h = np.mean([r[i] for r in self.historial_rostros if len(r) > i], axis=0).astype(int)
+            rostros_suavizados.append((x, y, w, h))
 
-    if len(rostros) > 0:
-        historial_rostros.append(rostros)
-    else:
-        historial_rostros.append([])
+        for (x, y, w, h) in rostros_suavizados:
+            sombrero_resized = cv2.resize(self.sombrero_img, (w, int(w * self.sombrero_img.shape[0] / self.sombrero_img.shape[1])))
+            alpha_resized = cv2.resize(self.alpha, (w, int(w * self.alpha.shape[0] / self.alpha.shape[1])))
 
-    if len(historial_rostros) > 5:
-        historial_rostros.pop(0)
+            max_sombrero_height = int(h * 0.6)
+            if sombrero_resized.shape[0] > max_sombrero_height:
+                sombrero_resized = cv2.resize(sombrero_resized, (w, max_sombrero_height))
+                alpha_resized = cv2.resize(alpha_resized, (w, max_sombrero_height))
 
-    rostros_suavizados = []
-    for i in range(len(rostros)):
-        x, y, w, h = np.mean([r[i] for r in historial_rostros if len(r) > i], axis=0).astype(int)
-        rostros_suavizados.append((x, y, w, h))
+            sombrero_x = x
+            sombrero_y = y - sombrero_resized.shape[0]
+            if sombrero_y < 0:
+                sombrero_y = 0
 
-    for (x, y, w, h) in rostros_suavizados:
-        sombrero_resized = cv2.resize(sombrero_img, (w, int(w * sombrero_img.shape[0] / sombrero_img.shape[1])))
-        alpha_resized = cv2.resize(alpha, (w, int(w * alpha.shape[0] / alpha.shape[1])))
+            h, w, _ = sombrero_resized.shape
+            sombrero_region = frame[sombrero_y:sombrero_y + h, sombrero_x:sombrero_x + w]
 
-        max_sombrero_height = int(h * 0.6)
-        if sombrero_resized.shape[0] > max_sombrero_height:
-            sombrero_resized = cv2.resize(sombrero_resized, (w, max_sombrero_height))
-            alpha_resized = cv2.resize(alpha_resized, (w, max_sombrero_height))
-        
-        sombrero_x = x
-        sombrero_y = y - sombrero_resized.shape[0]
-        if sombrero_y < 0:
-            sombrero_y = 0
+            for c in range(0, 3):
+                sombrero_region[:, :, c] = (
+                    alpha_resized * sombrero_resized[:, :, c] + (1 - alpha_resized) * sombrero_region[:, :, c]
+                )
 
-        h, w, _ = sombrero_resized.shape
-        sombrero_region = frame[sombrero_y:sombrero_y + h, sombrero_x:sombrero_x + w]
-        
-        for c in range(0, 3):
-            sombrero_region[:, :, c] = (
-                alpha_resized * sombrero_resized[:, :, c] + (1 - alpha_resized) * sombrero_region[:, :, c]
-            )
-        
-        frame[sombrero_y:sombrero_y + h, sombrero_x:sombrero_x + w] = sombrero_region
+            frame[sombrero_y:sombrero_y + h, sombrero_x:sombrero_x + w] = sombrero_region
 
-    return frame
+        return frame
 
-def main():
-    global current_sombrero_index, sombrero_img, alpha
-
+def main(filtros):
     cap = cv2.VideoCapture(0)
     
     if not cap.isOpened():
@@ -77,22 +71,27 @@ def main():
             print("No se pudo leer el frame del video.")
             break
         
-        frame = agregar_sombrero(frame, sombrero_img, alpha)
+        for filtro in filtros:
+            frame = filtro.agregar_sombrero(frame)
         
-        cv2.imshow("Filtro de Sombrero", frame)
+        cv2.imshow("Filtros", frame)
         
         key = cv2.waitKey(50) & 0xFF
         if key == ord('q'):
             break
         elif key == ord('d'):
-            current_sombrero_index = (current_sombrero_index + 1) % len(sombrero_imgs)
-            sombrero_img, alpha = cargar_sombrero(current_sombrero_index)
+            for filtro in filtros:
+                filtro.current_sombrero_index = (filtro.current_sombrero_index + 1) % len(filtro.sombrero_imgs)
+                filtro.sombrero_img, filtro.alpha = filtro.cargar_sombrero(filtro.current_sombrero_index)
         elif key == ord('a'):
-            current_sombrero_index = (current_sombrero_index - 1) % len(sombrero_imgs)
-            sombrero_img, alpha = cargar_sombrero(current_sombrero_index)
+            for filtro in filtros:
+                filtro.current_sombrero_index = (filtro.current_sombrero_index - 1) % len(filtro.sombrero_imgs)
+                filtro.sombrero_img, filtro.alpha = filtro.cargar_sombrero(filtro.current_sombrero_index)
 
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main()
+    sombrero_filtro = SombreroFiltro()
+    # Aquí se pueden agregar más filtros en la lista
+    main([sombrero_filtro])
